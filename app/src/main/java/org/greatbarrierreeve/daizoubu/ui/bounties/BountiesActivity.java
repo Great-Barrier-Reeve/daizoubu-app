b07652ddf2dae4bb66471ba19a2c0736348ba548
edwardcjd <edwardchan.jd@gmail.com>
2026-04-09 04:21:29 +0800

    feat(bounties): add cards to recycler view

--- app/src/main/java/org/greatbarrierreeve/daizoubu/ui/bounties/BountiesActivity.java
+++ app/src/main/java/org/greatbarrierreeve/daizoubu/ui/bounties/BountiesActivity.java
@@ -36,7 +36,7 @@ public class BountiesActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bounties);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bounties), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
