c86971c2219f9bab873fb802ee71f8baccfbc5a5
edwardcjd <edwardchan.jd@gmail.com>
2026-04-12 19:46:00 +0800

    feat(order): add button logic in cart activity to show location activity

--- app/src/main/java/org/greatbarrierreeve/daizoubu/ui/order/cart/CartActivity.java
+++ app/src/main/java/org/greatbarrierreeve/daizoubu/ui/order/cart/CartActivity.java
@@ -1,20 +1,24 @@
 package org.greatbarrierreeve.daizoubu.ui.order.cart;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import org.greatbarrierreeve.daizoubu.R;
+import org.greatbarrierreeve.daizoubu.ui.order.location.LocationActivity;
 
 
 public class CartActivity extends AppCompatActivity {
 
+    ConstraintLayout sectionDeliveryLocation;
     ImageView iconBack;
 
 
@@ -37,6 +41,10 @@ public class CartActivity extends AppCompatActivity {
         // back button click event handler
         iconBack = findViewById(R.id.iconBack);
         iconBack.setOnClickListener(view -> this.finish());
+
+        // delivery location section event handler
+        sectionDeliveryLocation = findViewById(R.id.sectionDeliveryLocation);
+        sectionDeliveryLocation.setOnClickListener(view -> startActivity(new Intent(CartActivity.this, LocationActivity.class)));
 
     }
 
