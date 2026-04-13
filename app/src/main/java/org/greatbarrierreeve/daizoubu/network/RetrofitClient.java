ec4fd02ca70c8c81d94d8cf13dc7b21e0d926da2
Michael <heansoh@gmail.com>
2026-04-13 22:43:45 +0800

    http Client header

--- app/src/main/java/org/greatbarrierreeve/daizoubu/network/RetrofitClient.java
+++ app/src/main/java/org/greatbarrierreeve/daizoubu/network/RetrofitClient.java
@@ -2,6 +2,13 @@ package org.greatbarrierreeve.daizoubu.network;
 
 import org.greatbarrierreeve.daizoubu.api.ErrandService;
 
+import java.io.IOException;
+
+import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -20,9 +27,24 @@ public class RetrofitClient {
     private static ErrandService service;
 
     public static ErrandService getService(){
+
+        OkHttpClient httpClient;
+
+        Interceptor headAuthInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                Headers header = request.headers().newBuilder().add("Authorization", "as").build();
+                request = request.newBuilder().headers(header).build();
+                return chain.proceed(request);
+            }
+        };
+        httpClient = new OkHttpClient.Builder().addInterceptor(headAuthInterceptor).build();
+
         if (service == null){
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(httpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
             service = retrofit.create(ErrandService.class);
